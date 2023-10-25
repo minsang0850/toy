48567da4332b6e0cdfc3b56db0421b6750930944
mingsang <minsang0850@naver.com>
2023-10-26 00:05:58 +0900

    Effective Java: Item 34

--- src/main/java/minsang/toy/effectivejava/chapter6/item34/PayrollDay.java
+++ src/main/java/minsang/toy/effectivejava/chapter6/item34/PayrollDay.java
@@ -14,12 +14,35 @@ public enum PayrollDay {
 
     private final PayType payType;
 
-    PayrollDay(PayType payType){
+    PayrollDay(PayType payType) {
         this.payType = payType;
     }
 
-    enum PayType{
-        WEEKDAY,
-        WEEKEND;
+    int pay(int minutesWorked, int payRate) {
+        return payType.pay(minutesWorked, payRate);
+    }
+
+    enum PayType {
+        WEEKDAY {
+            //주중에는 8시간 미만으로 일할 시 추가수당 없음.
+            public int overtimePay(int minutesWorked, int payRate) {
+                return minutesWorked <= MINS_PER_SHIFT ? 0 :
+                        (minutesWorked - MINS_PER_SHIFT) * payRate / 2;
+            }
+        },
+        WEEKEND{
+            //주말에는 기본적으로 추가수당. (시급의 0.5배를 추가로)
+            public int overtimePay(int minutesWorked, int payRate) {
+                return minutesWorked * payRate / 2;
+            }
+        };
+
+        abstract int overtimePay(int mins, int payRate);
+        private static final int MINS_PER_SHIFT = 8 * 60;
+
+        int pay(int minsWorked, int payRate) {
+            int basePay = minsWorked * payRate;
+            return basePay + overtimePay(minsWorked, payRate);
+        }
     }
 }
