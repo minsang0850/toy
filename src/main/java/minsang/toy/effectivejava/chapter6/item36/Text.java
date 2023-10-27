f0c6764ffc5c599e0d31c9d1f11d88c72b837c44
mingsang <minsang0850@naver.com>
2023-10-28 00:02:19 +0900

    Effective Java: Item 36

--- src/main/java/minsang/toy/effectivejava/chapter6/item36/Text.java
+++ src/main/java/minsang/toy/effectivejava/chapter6/item36/Text.java
@@ -1,5 +1,6 @@
 package minsang.toy.effectivejava.chapter6.item36;
 
+import java.util.EnumSet;
 import java.util.Set;
 
 public class Text {
@@ -7,6 +8,16 @@ public class Text {
 
     public void applyStyles(Set<Style> styles) {
         // ...
+    }
+
+    public EnumSet<Style> allOf(){
+        //Color의 모든 요소를 포함하는 EnumSet
+        return EnumSet.allOf(Style.class);
+    }
+
+    public EnumSet<Style> noneOf(){
+        //Color의 빈 Set
+        return EnumSet.noneOf(Style.class);
     }
 }
 
